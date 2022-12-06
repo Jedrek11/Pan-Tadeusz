a06874a162466cdf37a2e6e1bb572c181585ce54
Jedrek11 <jedrekpawlowski011@gmail.com>
2022-12-06 10:31:54 +0100

    changed index.html into index.php

--- index.php
+++ index.php
@@ -19,21 +19,24 @@
                 <div class="row">
                     <ul class="col-4">
                         <h2>Spis treści</h2>
-                        <li><a href="./k1.html">Księga 1</a></li>
-                        <li><a href="./k2.html">Księga 2</a></li>
-                        <li><a href="./k3.html">Księga 3</a></li>
-                        <li><a href="./k4.html">Księga 4</a></li>
-                        <li><a href="./k5.html">Księga 5</a></li>
-                        <li><a href="./k6.html">Księga 6</a></li>
-                        <li><a href="./k7.html">Księga 7</a></li>
-                        <li><a href="./k8.html">Księga 8</a></li>
-                        <li><a href="./k9.html">Księga 9</a></li>
-                        <li><a href="./k10.html">Księga 10</a></li>
-                        <li><a href="./k11.html">Księga 11</a></li>
-                        <li><a href="./k12.html">Księga 12</a></li>
+                        <li><a href="./index.php"> Strona główna </li>
+                        <?php
+                          for ($k=1; $k <= 12 ; $k++) { 
+                              print("<li><a href='./index.php?k=$k&name=Ala'>Księga $k</a></li>");
+                          }
+                        ?>
+                  
                     </ul>
                     <div class="col-8">
-                        <img src="./pan_tadeusz.jpg" alt="Pan Tadeusz">
+                        <?php
+                            if (isset($_GET["k"])) {
+                                $k = $_GET['k'];
+                                include_once("k$k.html");
+                            } else{
+                            print('<img src="./pan_tadeusz.jpg" alt="Pan Tadeusz">');
+                            }
+                        ?>
+                    
                     </div>
                 </div>
             </div>
